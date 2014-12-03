2ae32ec718af1c123f1df57de678e12bef58029e
Silviu Lipovan <silviu.lipovan@shazam.com>
2014-12-03 14:39:59 +0000

    added extension parameters for fork

--- src/main/groovy/com/shazam/fork/gradle/ForkExtension.groovy
+++ src/main/groovy/com/shazam/fork/gradle/ForkExtension.groovy
@@ -17,10 +17,27 @@ package com.shazam.fork.gradle
  */
 class ForkExtension {
 
-    /** Output directory for Fork report files. If empty, the default dir will be used */
-    File baseOutputDir
+    /** Debug logging flag. */
+    boolean debug
+
+    /** Fail if no device is connected flag. */
+    boolean failIfNoDeviceConnected
 
     /** Ignore test failures flag. */
     boolean ignoreFailures
 
+    /** Fully qualified name of the test class to be run (e.g. com.example.foo.test.MyTestCase). */
+    String className
+
+    /** Test method to be run. Used when `className` is provided. */
+    String methodName
+
+    /** Devices to run the tests on (specified with serial numbers). */
+    Set<String> devices
+
+    /** Whether or not animations are enabled, useful for slow machines or projects with many screenshots */
+    boolean noAnimations
+
+    /** Output directory for the spoon report files. If empty, the default dir will be used */
+    File baseOutputDir
 }
